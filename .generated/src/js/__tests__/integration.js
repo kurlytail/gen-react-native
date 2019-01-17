1864e367de07d53ee9fc1ff52ed785ca9e2e2d68
ashwin <ashwinkn@hotmail.com>
2019-01-17 12:13:25 -0600

    Skeleton generator framework

---
+++ .generated/src/js/__tests__/integration.js
@@ -0,0 +1,39 @@
+import { execSync } from 'child_process';
+
+
+
+describe('# integration test', () => {
+    beforeEach(() => {
+        execSync('rm -rf testoutput');
+    });
+
+    it('## should print help options', () => {
+        const output = execSync('./scripts/sgen-react-native.sh -h').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design', () => {
+        const output = execSync('./scripts/sgen-react-native.sh -d src/test/fixture/design.js -o testoutput').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design with merge', () => {
+        let output = execSync(
+            './scripts/sgen-react-native.sh -d src/test/fixture/design.js -o testoutput --overwrite=merge'
+        ).toString();
+        expect(output).toMatchSnapshot();
+        output = execSync(
+            './scripts/sgen-react-native.sh -d src/test/fixture/design.js -o testoutput --overwrite=merge'
+        ).toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design and run react-native commands', () => {
+        let output = execSync(
+            './scripts/sgen-react-native.sh -d src/test/fixture/design.js -o testoutput --overwrite=merge'
+        ).toString();
+        expect(output).toMatchSnapshot();
+        output = execSync('npm install', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
+    });
+});
